59f36ecea15dcc5906e491f8b6101ee271e17b5c
ShihabReza <shihabreza457@gmail.com>
2022-05-21 22:12:57 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './Page/Sheard/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Page/Home/Home';
+import Purchase from './Page/Purchase/Purchase';
+import Dashboard from './Page/Dashboard/Dashboard';
+import Login from './Page/Login/Login';
+import Blog from './Page/Blog/Blog';
 
 function App() {
   return (
     <div className="App">
-        <h1 className="text-2xl text-primary font-bold">Start My WebSite</h1>
+        <Navbar>
+            <Routes>
+                <Route path="/" element={<Home></Home>}></Route>
+                <Route path="/Purchase" element={<Purchase></Purchase>}></Route>
+                <Route path="/Dashbord" element={<Dashboard></Dashboard>}></Route>
+                <Route path="/blog" element={<Blog></Blog>}></Route>
+                <Route path="/Login" element={<Login></Login>}></Route>
+            </Routes>
+        </Navbar>
     </div>
   );
 }
---
+++ src/Page/Purchase/Purchase.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Purchase = () => {
+    return (
+        <div>
+            <h1>This Is Purchase</h1>
+        </div>
+    );
+};
+
+export default Purchase;
